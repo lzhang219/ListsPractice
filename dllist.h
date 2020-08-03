6b583cebea56a1413522f8563fae2595a0725686
Leon Zhang <lzhang219@gmail.com>
2020-08-03 19:38:22 -0400

    New file dllist.h

---
+++ dllist.h
@@ -0,0 +1,13 @@
+//
+// Created by l05zh on 8/3/2020.
+//
+
+#ifndef LISTSPRACTICE_DLLIST_H
+#define LISTSPRACTICE_DLLIST_H
+struct DLListNode{
+
+};
+struct DLList{
+
+};
+#endif //LISTSPRACTICE_DLLIST_H
