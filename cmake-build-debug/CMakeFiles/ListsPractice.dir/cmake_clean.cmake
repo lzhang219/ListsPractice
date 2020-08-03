0f0d33e8b42dbf9df6b523fb27c0213244d05082
Leon Zhang <lzhang219@gmail.com>
2020-08-03 14:00:20 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ListsPractice.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ListsPractice.dir/main.cpp.obj"
+  "ListsPractice.exe"
+  "ListsPractice.exe.manifest"
+  "ListsPractice.pdb"
+  "libListsPractice.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ListsPractice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
