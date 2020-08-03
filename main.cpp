5020ac2993afaafc1784aff4975c221425702d70
Leon Zhang <lzhang219@gmail.com>
2020-08-03 18:20:18 -0400

    got rid of hello world

--- main.cpp
+++ main.cpp
@@ -6,11 +6,9 @@ int main() {
     n.modElement(30,1);
     n.InsElement(40,4);
     n.removeElement(23);
-
     for (int i = 0; i <n.size(); ++i) {
         cout<<n.getElement(i)<<' ';
     }
     cout<<endl;
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
