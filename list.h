4cd28e4dbe9f4d2fb6b2c5314f2f142fe169b654
Leon Zhang <lzhang219@gmail.com>
2020-08-03 16:11:12 -0400

    Removed list.cpp, moved to list.h.

--- list.h
+++ list.h
@@ -5,25 +5,73 @@
 #ifndef LISTSPRACTICE_LIST_H
 #define LISTSPRACTICE_LIST_H
 
+
 template<class T>
-struct ListNode
-{
+struct ListNode {
 public:
     T key;
-    ListNode * next;
-    ListNode();
-    explicit ListNode(T key);
-    ListNode(T key, ListNode *next);
+    ListNode *next;
 
-    virtual ~ListNode();
+    ListNode() {
+        next = nullptr;
+    }
+
+    explicit ListNode(T key) : key(key) {}
+
+    ListNode(T key, ListNode *next) : key(key), next(next) {}
+
+    virtual ~ListNode() {
+        if (next != nullptr) {
+            delete next;
+        }
+    }
 };
-template <class T>
-struct List{
+
+template<class T>
+struct List {
     ListNode<T> *first, *last;
-    List();
-    List(int length, T t);
-    void InsElementFront(T t);
-    void InsElementEnd(T t);
-    void modElement(int i, T t);
+
+    List() {
+        first = new ListNode<T>;
+        last = first;
+    }
+
+    List(int length, T t) {
+        first = new ListNode<T>(t);
+        ListNode<T> *current = first;
+        --length;
+        while (length--) {
+            current->next = new ListNode<T>(t);
+            current = current->next;
+        }
+        last = current;
+    }
+
+    ~List() {
+        delete first;
+    }
+
+    void InsElementFront(T t) {
+        auto *current = new ListNode<T>(t);
+        current->next = first;
+        first = current;
+    }
+
+    void InsElementEnd(T t) {
+        last = last->next = new ListNode<T>(t);
+    }
+
+    void modElement(int i, T t) {
+        if (!i) {
+            first->key = t;
+            return;
+        }
+        auto *current = first;
+        while (i--) {
+            current = current->next;
+        }
+        current->key = t;
+    }
 };
+
 #endif //LISTSPRACTICE_LIST_H
